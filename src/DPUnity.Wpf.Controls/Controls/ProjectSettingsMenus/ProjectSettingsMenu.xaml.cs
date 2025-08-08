e0b2dc0fa70dccbead5549c4cdf6243327f904d1
LKV <lakhavanzz@gmail.com>
2025-08-08 15:40:34 +0700

    Add project settings interface and user control
    Updated project file to include versioning and removed unnecessary item groups. Added `IDP_ProjectSetting` interface with relevant properties and methods. Created `ProjectSettingsMenu.xaml` for a user control with a styled `ListView` for managing project settings, along with its code-behind to handle data binding and commands.

---
+++ src/DPUnity.Wpf.Controls/Controls/ProjectSettingsMenus/ProjectSettingsMenu.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DPUnity.Wpf.Controls.Controls.ProjectSettingsMenus
+{
+    /// <summary>
+    /// Interaction logic for ProjectSettingsMenu.xaml
+    /// </summary>
+    public partial class ProjectSettingsMenu : UserControl
+    {
+        public ProjectSettingsMenu()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(ProjectSettingsMenu));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(object), typeof(ProjectSettingsMenu));
+
+        public object SelectedItem
+        {
+            get { return GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty AddNewCommandProperty =
+            DependencyProperty.Register("AddNewCommand", typeof(ICommand), typeof(ProjectSettingsMenu));
+
+        public ICommand AddNewCommand
+        {
+            get { return (ICommand)GetValue(AddNewCommandProperty); }
+            set { SetValue(AddNewCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty SaveCommandProperty =
+            DependencyProperty.Register("SaveCommand", typeof(ICommand), typeof(ProjectSettingsMenu));
+
+        public ICommand SaveCommand
+        {
+            get { return (ICommand)GetValue(SaveCommandProperty); }
+            set { SetValue(SaveCommandProperty, value); }
+        }
+    }
+}
